d1b006deebfbca481e486b9d545275ad49d8a0d3
Clauvin <clauvin_almeida@hotmail.com>
2018-12-08 02:31:27 -0200

    Copy/pasting Priscila's project

---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Public variables
+    public enum States
+    {
+        Idle,
+        Walk,
+        Attack
+    }
+    public States state;
+
+    public GameObject player;
+    public float minDistanceToAttack = 1.5f;
+    public float minDistanceToWalk = 20.0f;
+    public float speedRotation = 1.0f;
+    public float speedWalk = 1.0f;
+
+    //Private variables
+    private Animator anim;
+    private bool rotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = States.Idle;
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        StartCoroutine(UpdateFSM());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (state == States.Walk)
+        {
+            UpdateRotation(player);
+            UpdatePosition();
+        }
+
+        if (state == States.Attack)
+            UpdateRotation(player);
+	}
+
+    IEnumerator UpdateFSM()
+    {
+        while (true)
+        {
+            if (state == States.Idle)
+            {
+                if (PlayerIsVisible())
+                {
+                    state = States.Walk;
+                    Walk();
+                }
+
+                if (PlayerIsNear())
+                {
+                    state = States.Attack;
+                    Attack();
+                }
+            }
+            else
+            {
+                if (state == States.Walk)
+                {
+                    if (!PlayerIsVisible())
+                    {
+                        state = States.Idle;
+                        Idle();
+                    }
+
+                    if (PlayerIsNear())
+                    {
+                        state = States.Attack;
+                        Attack();
+                    }
+                }
+                else if (state == States.Attack)
+                {
+                    if (!PlayerIsNear())
+                    {
+                        state = States.Walk;
+                        Walk();
+                    }
+                    if (!PlayerIsVisible())
+                    {
+                        state = States.Idle;
+                        Idle();
+                    }
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private bool PlayerIsNear()
+    {
+        Vector3 distance = transform.position - player.transform.position;
+        if (distance.magnitude <= minDistanceToAttack)
+            return true;
+        else
+            return false;
+    }
+
+    private bool PlayerIsVisible()
+    {
+        Vector3 distance = transform.position - player.transform.position;
+        if (distance.magnitude <= minDistanceToWalk)
+            return true;
+        else
+            return false;
+    }
+    
+    private void Idle()
+    {
+        anim.SetBool("enemyAttack", false);
+        anim.SetBool("enemyWalk", false);
+    }
+
+    private void Walk()
+    {
+        anim.SetBool("enemyWalk", true);
+        anim.SetBool("enemyAttack", false);
+    }
+
+    private void Attack()
+    {
+        anim.SetBool("enemyWalk", true);
+        anim.SetBool("enemyAttack", true);
+    }
+
+    private void UpdateRotation(GameObject target)
+    {
+        Vector3 relativePosition = target.transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(relativePosition);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * speedRotation);
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position += transform.forward * Time.deltaTime * speedWalk;
+    }
+}
---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private variables
+    private Animator anim;
+    private float moveZ;
+    private float moveX;
+
+    private int hash;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Moving the character with arrow keys
+        moveZ = Input.GetAxis("Vertical");
+        moveX = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", moveZ);
+        anim.SetFloat("Turn", moveX);
+
+        // Attack
+        if (Input.GetKeyDown("space"))
+            anim.SetTrigger("Attack");
+    }
+}
