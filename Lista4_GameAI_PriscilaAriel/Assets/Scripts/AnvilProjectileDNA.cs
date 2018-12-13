ba6b1e0b0af4463fd6ef23dff9088de3fdd83fe0
Clauvin <clauvin_almeida@hotmail.com>
2018-12-13 18:39:30 -0200

    (#5) Removed enum of AnvilProjectileDNA

--- Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilProjectileDNA.cs
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilProjectileDNA.cs
@@ -6,23 +6,6 @@ public class AnvilProjectileDNA : MonoBehaviour {
 
     public float[] projectile_DNA;
 
-    enum ProjectileDNANames
-    {
-        PROJECTILE_SPEED,
-        X_SIZE,
-        Y_SIZE,
-        Z_SIZE,
-        X_OFFSET,
-        Y_OFFSET,
-        Z_OFFSET,
-        X_EXTRA_ACCELERATION,
-        Y_EXTRA_ACCELERATION,
-        Z_EXTRA_ACCELERATION,
-        X_TELEGUIDING,
-        Y_TELEGUIDING,
-        Z_TELEGUIDING
-    }
-
     AnvilProjectileDNA(float[] projectile_DNA)
     {
         this.projectile_DNA = projectile_DNA;
