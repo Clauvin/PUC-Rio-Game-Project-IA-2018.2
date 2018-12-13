c6595993640c85f7b95f06722ddf5d6c3e8dd75f
Clauvin <clauvin_almeida@hotmail.com>
2018-12-13 21:47:43 -0200

    (#5) Creation of AnvilProjectileDestructionData

---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilProjectileDestructionData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilProjectileDestructionData : MonoBehaviour {
+
+    public float time_of_creation;
+    public float how_many_seconds_it_should_exist;
+
+}
