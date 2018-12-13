941087b1a814840b3c14a62918af0799cae09a83
Clauvin <clauvin_almeida@hotmail.com>
2018-12-13 18:04:48 -0200

    (#5) Created AnvilProjectileDNA

---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilProjectileDNA.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilProjectileDNA : MonoBehaviour {
+
+    float[] projectile_DNA;
+
+    enum ProjectileDNANames
+    {
+        PROJECTILE_SPEED,
+        X_SIZE,
+        Y_SIZE,
+        Z_SIZE,
+        X_OFFSET,
+        Y_OFFSET,
+        Z_OFFSET,
+        X_EXTRA_ACCELERATION,
+        Y_EXTRA_ACCELERATION,
+        Z_EXTRA_ACCELERATION,
+        X_TELEGUIDING,
+        Y_TELEGUIDING,
+        Z_TELEGUIDING
+    }
+
+    AnvilProjectileDNA(float[] projectile_DNA)
+    {
+        this.projectile_DNA = projectile_DNA;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
