8ee7efd366c640fd727ebc597f42fbc701611d96
Clauvin <clauvin_almeida@hotmail.com>
2018-12-13 21:48:11 -0200

    (#5) Created AnvilProjectileFitnessData

---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilProjectileFitnessData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilProjectileFitnessData : MonoBehaviour {
+
+    public float fitness;
+
+	// Use this for initialization
+	void Start () {
+        fitness = Mathf.Infinity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
