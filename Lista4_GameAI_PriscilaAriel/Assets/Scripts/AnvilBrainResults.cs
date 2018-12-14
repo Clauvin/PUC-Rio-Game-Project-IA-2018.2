41f9d5b752967fffb7a739f0015188dd77326abe
Clauvin <clauvin_almeida@hotmail.com>
2018-12-14 01:11:41 -0200

    (#5) Update of AnvilBrainResults

--- Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilBrainResults.cs
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilBrainResults.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Support;
 
 public class AnvilBrainResults : MonoBehaviour {
 
@@ -9,11 +10,49 @@ public class AnvilBrainResults : MonoBehaviour {
     public int best_dna;
     public float best_fitness;
 
-	// Use this for initialization
-	void Start () {
-        dnas = new ArrayList();
-        fitness_data = new ArrayList();
+    public void TrimLists()
+    {
+        AnvilProjectileDNA dna = (AnvilProjectileDNA)dnas[best_dna];
+        float fitness = best_fitness;
+
+        dnas = new ArrayList(1);
+        dnas.Add(dna);
         best_dna = 0;
+        best_fitness = fitness;
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        best_dna = 0;
+        best_fitness = Mathf.Infinity;
+    }
+
+    void Start () {
+        dnas = new ArrayList(1);
+        fitness_data = new ArrayList(1);
+
+
+        float[] valores = new float[14];
+        valores[(int)ProjectileDNANames.PROJECTILE_TIME] = 600.0f;
+        valores[(int)ProjectileDNANames.PROJECTILE_SPEED] = 5;
+        valores[(int)ProjectileDNANames.X_EXTRA_ACCELERATION] = 0;
+        valores[(int)ProjectileDNANames.X_OFFSET] = 0;
+        valores[(int)ProjectileDNANames.X_SIZE] = 2;
+        valores[(int)ProjectileDNANames.X_TELEGUIDING] = 0;
+        valores[(int)ProjectileDNANames.Y_EXTRA_ACCELERATION] = 0;
+        valores[(int)ProjectileDNANames.Y_OFFSET] = 0;
+        valores[(int)ProjectileDNANames.Y_SIZE] = 2;
+        valores[(int)ProjectileDNANames.Y_TELEGUIDING] = 0;
+        valores[(int)ProjectileDNANames.Z_EXTRA_ACCELERATION] = 0;
+        valores[(int)ProjectileDNANames.Z_OFFSET] = 0;
+        valores[(int)ProjectileDNANames.Z_SIZE] = 2;
+        valores[(int)ProjectileDNANames.Z_TELEGUIDING] = 0;
+
+        AnvilProjectileDNA starter_dna = new AnvilProjectileDNA(valores);
+
+        dnas.Add(starter_dna);
+        fitness_data.Add(new AnvilProjectileFitnessData());
 	}
 	
 	// Update is called once per frame
