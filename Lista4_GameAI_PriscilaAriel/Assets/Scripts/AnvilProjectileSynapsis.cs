a0f86cb34bdf8fefb079d9fb46dfcc567d530385
Clauvin <clauvin_almeida@hotmail.com>
2018-12-13 22:06:04 -0200

    (#5) Created AnvilProjectileSynapsis

---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilProjectileSynapsis.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilProjectileSynapsis : MonoBehaviour {
+
+    AnvilBrainResults brain_results;
+
+    void PassResultsToBrain()
+    {
+        brain_results.dnas.Add(GetComponent<AnvilProjectileDNA>());
+        brain_results.fitness_data.Add(GetComponent<AnvilProjectileFitnessData>());
+
+        if (brain_results.dnas.Count == 1)
+        {
+            brain_results.best_dna = 0;
+            brain_results.best_fitness = GetComponent<AnvilProjectileFitnessData>().fitness;
+        } else
+        {
+            if (brain_results.best_fitness > GetComponent<AnvilProjectileFitnessData>().fitness)
+            {
+                brain_results.best_dna = brain_results.dnas.Count - 1;
+                brain_results.best_fitness = GetComponent<AnvilProjectileFitnessData>().fitness;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        brain_results = GameObject.FindGameObjectWithTag("Anvil Brain").GetComponent<AnvilBrainResults>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
