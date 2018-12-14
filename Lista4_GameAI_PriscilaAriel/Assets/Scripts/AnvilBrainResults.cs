9d3a421632e0d8ba8da5483da5142360eb4a294d
Clauvin <clauvin_almeida@hotmail.com>
2018-12-13 22:06:43 -0200

    (#5) Created AnvilBrainResults

---
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilBrainResults.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilBrainResults : MonoBehaviour {
+
+    public ArrayList dnas;
+    public ArrayList fitness_data;
+    public int best_dna;
+    public float best_fitness;
+
+	// Use this for initialization
+	void Start () {
+        dnas = new ArrayList();
+        fitness_data = new ArrayList();
+        best_dna = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
