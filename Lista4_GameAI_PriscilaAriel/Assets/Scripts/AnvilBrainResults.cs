ac69934375705775c9006ff8cf222cde2f8aa083
Clauvin <clauvin_almeida@hotmail.com>
2018-12-17 21:23:31 -0200

    (#10) Add ChooseBestBestDNAandFitness()

--- Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilBrainResults.cs
+++ Lista4_GameAI_PriscilaAriel/Assets/Scripts/AnvilBrainResults.cs
@@ -7,37 +7,61 @@ public class AnvilBrainResults : MonoBehaviour {
 
     public ArrayList dnas;
     public ArrayList fitness_data;
-    public int best_dna;
-    public float best_fitness;
+
+    public AnvilProjectileDNA best_dna;
+    public float best_fitness = float.MaxValue;
+
+    public int quant_of_worsening_fitness = 0;
+
+    public void ChooseBestBestDNAandFitness()
+    {
+        float fit = float.MaxValue;
+        int position = 0;
+
+        for (int i = 0; i < dnas.Count; i++)
+        {
+            if (fit > ((AnvilProjectileFitnessData)fitness_data[i]).fitness)
+            {
+                fit = ((AnvilProjectileFitnessData)fitness_data[i]).fitness;
+                position = i;
+            }
+        }
+
+        if (best_dna == null)
+        {
+            best_dna = new AnvilProjectileDNA(((AnvilProjectileDNA)dnas[position]).getProjectileDNA());
+            best_fitness = fit;
+        }
+        else if (best_fitness > fit)
+        {
+            best_dna = new AnvilProjectileDNA(((AnvilProjectileDNA)dnas[position]).getProjectileDNA());
+            best_fitness = fit;
+        }
+        else
+        {
+            quant_of_worsening_fitness++;
+        }
+
+        Debug.Log(best_dna);
+    }
 
     public void TrimLists()
     {
-        AnvilProjectileDNA dna = (AnvilProjectileDNA)dnas[best_dna];
-        float fitness = best_fitness;
-
-        if (fitness > ((AnvilProjectileFitnessData)fitness_data[best_dna]).fitness)
-            fitness = ((AnvilProjectileFitnessData)fitness_data[best_dna]).fitness;
-
         dnas = new ArrayList(1);
-        dnas.Add(dna);
-        best_dna = 0;
-        best_fitness = fitness;
+        dnas.Add(best_dna);
     }
-
-
 
     // Use this for initialization
     void Awake()
     {
-        best_dna = 0;
-        best_fitness = Mathf.Infinity;
+
     }
 
     void Start () {
         dnas = new ArrayList(1);
         fitness_data = new ArrayList(1);
 
-        float[] valores = new float[14];
+        float[] valores = new float[11];
         valores[(int)ProjectileDNANames.PROJECTILE_TIME] = 3.0f;
         valores[(int)ProjectileDNANames.PROJECTILE_SPEED] = 5;
         valores[(int)ProjectileDNANames.X_OFFSET] = 0;
@@ -53,6 +77,7 @@ public class AnvilBrainResults : MonoBehaviour {
         AnvilProjectileDNA starter_dna = new AnvilProjectileDNA(valores);
 
         dnas.Add(starter_dna);
+        best_dna = starter_dna;
         fitness_data.Add(new AnvilProjectileFitnessData());
 	}
 	
